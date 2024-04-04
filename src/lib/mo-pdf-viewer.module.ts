4d4947c26311dc6f822e007026402a0de14dd7b2
tnewton <tnewton@modusoperandi.com>
2024-04-04 18:35:28 -0400

    [AT-236] - Removed browserAnimationModule and replace it with the CommonModule and animation provider

--- src/lib/mo-pdf-viewer.module.ts
+++ src/lib/mo-pdf-viewer.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { MoPdfViewerComponent } from './mo-pdf-viewer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     MoPdfViewerComponent
   ],
   imports: [
+    CommonModule,
     PdfViewerModule,
     FontAwesomeModule,
-    BrowserAnimationsModule,
     FormsModule
   ],
   exports: [
     MoPdfViewerComponent
-  ]
+  ],
+  providers: [ provideAnimations() ]
 })
 export class MoPdfViewerModule {}
